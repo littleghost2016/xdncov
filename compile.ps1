da542bf09d0ef082df6492f5387af25f1d556be5
littleghost2016 <785340571@qq.com>
2020-06-29 11:37:54 +0800

    增加对arm64的编译

--- compile.ps1
+++ compile.ps1
@@ -2,8 +2,11 @@ $env:GOARCH="amd64"
 $env:GOOS="windows"
 go build -o xdncov_windows_amd64.exe
 
+$env:GOOS="darwin"
+go build -o xdncov_darwin_amd64
+
 $env:GOOS="linux"
 go build -o xdncov_linux_amd64
 
-$env:GOOS="darwin"
-go build -o xdncov_darwin_amd64
+$env:GOARCH="arm64"
+go build -o xdncov_linux_arm64
